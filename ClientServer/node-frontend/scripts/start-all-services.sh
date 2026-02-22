aef85e0aef9ed55ea8444eb4e7c6f3e4fd7891bb
Mustafa <mohammedmustafa5205@gmail.com>
2026-02-21 21:16:28 -0500

    using venv for grpc

--- ClientServer/node-frontend/scripts/start-all-services.sh
+++ ClientServer/node-frontend/scripts/start-all-services.sh
@@ -41,6 +41,7 @@ export RMI_HOST="${RMI_HOST:-127.0.0.1}"
 export RMI_PORT="${RMI_PORT:-8101}"
 export BRIDGE_PORT="${BRIDGE_PORT:-8201}"
 export RUST_SERVER_PORT="${RUST_SERVER_PORT:-8001}"
+export PYTHON_BIN="${PYTHON_BIN:-python3}"
 
 start_proc "java tcp backend" bash -lc "cd '$ROOT_DIR' && java server"
 start_proc "rust tcp backend" bash -lc "cd '$ROOT_DIR' && ./rust-server"
@@ -48,7 +49,7 @@ start_proc "java rmi backend" bash -lc "cd '$ROOT_DIR/JavaRMI' && java server"
 
 wait_for_port "127.0.0.1" "$RMI_PORT" 40
 start_proc "java rmi bridge" bash -lc "cd '$ROOT_DIR/JavaRMI' && java rmi_bridge"
-start_proc "python grpc backend" bash -lc "cd '$ROOT_DIR/pythonGRPC' && python3 server.py"
+start_proc "python grpc backend" bash -lc "cd '$ROOT_DIR/pythonGRPC' && '$PYTHON_BIN' server.py"
 
 sleep 1
 start_proc "node frontend" bash -lc "cd '$ROOT_DIR/node-frontend' && node server.js"
